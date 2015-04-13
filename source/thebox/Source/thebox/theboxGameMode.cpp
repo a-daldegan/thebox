37df94a2425a5869e53f753fcda689f1cd1d2d43
Andrei Daldegan <andrei.daldegan@gmail.com>
2015-04-13 05:24:50 -0300

    ADD: Basic Multiplayer (Replicated Player Character)

--- source/thebox/Source/thebox/theboxGameMode.cpp
+++ source/thebox/Source/thebox/theboxGameMode.cpp
@@ -9,7 +9,7 @@ AtheboxGameMode::AtheboxGameMode(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/thebox/blueprints/PlayerCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
